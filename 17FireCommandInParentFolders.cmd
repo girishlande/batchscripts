1d8b09ca8a962cd2493204045b451dc477cb2dd9
AGILiAD\Girish.Lande <girish.lande@agiliad.com>
2020-04-27 11:37:03 +0530

    Updated script 17

--- 17FireCommandInParentFolders.cmd
+++ 17FireCommandInParentFolders.cmd
@@ -7,14 +7,18 @@ setlocal
 :: get current directory where this script resides 
 set back=%cd%
 echo current directory is %back%
+set dirpath=%cd%
 
 :: Get parent of current directory 
 for %%a in ("%dirpath%") do set "p_dir=%%~dpa"
-for %%a in (%p_dir:~0,-1%) do set "p2_dir=%%~dpa"
-echo Parent directory is %p2_dir%
+
+echo parent directory is %p_dir%
+
+::for %%a in (%p_dir:~0,-1%) do set "p2_dir=%%~dpa"
+::echo Parent directory is %p2_dir%
 
 echo All directories within this parent directory are 
-for /d %%i in (%p2_dir%*) do (
+for /d %%i in (%p_dir%*) do (
 echo %%i
 pushd %%i
 git pull
