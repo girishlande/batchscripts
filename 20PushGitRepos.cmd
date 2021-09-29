a630348c7d2cc40317ab0f7bff6faa769efdca32
Girish Lande <girishlande@gmail.com>
2021-09-29 16:18:14 +0530

    updated

---
+++ 20PushGitRepos.cmd
@@ -0,0 +1,29 @@
+:: Script to check git status of all repositories 
+:: This will tell us if we have uncommited changes in any of the repository 
+@echo off
+
+setlocal
+
+:: get current directory where this script resides 
+set back=%cd%
+echo current directory is %back%
+set dirpath=%cd%
+
+:: Get parent of current directory 
+for %%a in ("%dirpath%") do set "p_dir=%%~dpa"
+
+echo parent directory is %p_dir%
+
+::for %%a in (%p_dir:~0,-1%) do set "p2_dir=%%~dpa"
+::echo Parent directory is %p2_dir%
+
+echo All directories within this parent directory are 
+for /d %%i in (%p_dir%*) do (
+echo %%i
+pushd %%i
+git add *
+git commit -m "updated"
+git push origin
+popd
+)
+cd %back%
